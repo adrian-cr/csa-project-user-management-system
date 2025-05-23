ae7b4e80395f6963d26cc843205479d1cc2a0007
adrian-cr <acr77frb@gmail.com>
2025-05-22 22:57:05 -0600

    Added project setup files.

---
+++ src/main/java/com/cognizant/UserManagementSystem/UserManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.UserManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemApplication.class, args);
+	}
+
+}
